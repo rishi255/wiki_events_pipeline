86c3de667435b03ec1c37e2e1d39bb6c417c626f
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2026-05-02 01:57:22 +0530

    🧱 Split separate dockers into microservices

--- misc/useful_commands.sh
+++ misc/useful_commands.sh
@@ -3,3 +3,15 @@ uv export --group producer --no-dev --format requirements.txt --output-file prod
 
 # export consumer requirements.txt from uv
 uv export --group consumer --no-dev --format requirements.txt --output-file consumer/requirements.txt
+
+# Start infrastructure first (Kafka broker)
+docker compose -f docker-compose.infra.yml up
+
+# Then ingestion (Kafka producer)
+docker compose -f docker-compose.ingestion.yml up
+
+# Then compute stack (Kafka consumer(s))
+docker compose -f docker-compose.compute.yml up
+
+# Finally analytics stack (ClickHouse DB and Grafana)
+docker compose -f docker-compose.analytics.yml up
